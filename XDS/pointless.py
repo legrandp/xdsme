686802f29404e4fde37436e2cad1340ffe8c066d
Pierre Legrand <Pierre Legrand@localhost>
2010-03-23 08:42:07 +0100

    Fix a fex bugs in pointless results processing in case of P1 symmetry.

--- XDS/pointless.py
+++ XDS/pointless.py
@@ -20,40 +20,38 @@ def is_pointless_installed():
         return False
     os.system("rm -f %s" % tmpfile)
 
-def pointless(dir_name, hklinp="XDS_ASCII.HKL"):
-    # Run pointless and extract pointgroup and spacegroup determination
-    # from its xmlout file.
-    cmline = "pointless XDSIN %s XMLOUT XDS_pointless.xml"
-    cmline += " HKLOUT XDS_pointless.mtz > XDS_pointless.log"
-    os.chdir(dir_name)
-    os.system(cmline % hklinp)
-
+def process_pointless_xml():
     xml_inp = "XDS_pointless.xml"
     likely_spacegroups = []
     prob_max = 0
+    zone_list = []
+    get_elem = lambda n, m, f: f(
+                n.getElementsByTagName(m)[0].childNodes[0].data.strip())
     try:
         dom = minidom.parse(xml_inp)
-        get_elem = lambda n, m, f: f(
-                   n.getElementsByTagName(m)[0].childNodes[0].data.strip())
         cell = dom.getElementsByTagName('cell')[0]
-        zone_list = dom.getElementsByTagName('ZoneScoreList')[0]
         spg_list = dom.getElementsByTagName('SpacegroupList')[0]
     except:
         os.chdir("..")
         raise 
     cell_par = dict([(x, get_elem(cell, x, float)) for x in ('a', 'b', 'c')])
+    try:
+        zone_list = dom.getElementsByTagName('ZoneScoreList')[0]
+    except:
+        pass
     # Looking at systematique extinctions
-    print "\n  Systematique extinctions from pointless:"
-    print "  Zone Type            axe len.    #obs     Condition    Prob."
-    print "  "+60*"-"
-    for node in zone_list.getElementsByTagName('Zone'):
-        ztype = get_elem(node, 'ZoneType', str)
-        nobs = get_elem(node, 'Nobs', int)
-        prob = get_elem(node, 'Prob', float)
-        condition = get_elem(node, 'Condition', str)
-        axe = cell_par[ztype[ztype.index("[")+1:ztype.index("]")]]
-        all_dat = (ztype, axe, nobs, condition, prob)
-        print "%21s %8.1f Å %6d  %12s  %7.3f" % all_dat
+    if zone_list:
+        print "\n  Systematique extinctions from pointless:"
+        print "  Zone Type            axe len.    #obs     Condition    Prob."
+        print "  "+60*"-"
+        for node in zone_list.getElementsByTagName('Zone'):
+            ztype = get_elem(node, 'ZoneType', str)
+            nobs = get_elem(node, 'Nobs', int)
+            prob = get_elem(node, 'Prob', float)
+            condition = get_elem(node, 'Condition', str)
+            axe = cell_par[ztype[ztype.index("[")+1:ztype.index("]")]]
+            all_dat = (ztype, axe, nobs, condition, prob)
+            print "%21s %8.1f Å %6d  %12s  %7.3f" % all_dat
     print "\n  Possible spacegroup from pointless:"
     print "  Symbol      num   TotalProb   SysAbsProb"
     print "  "+40*"-"
@@ -72,11 +70,25 @@ def pointless(dir_name, hklinp="XDS_ASCII.HKL"):
     os.chdir("..")
     print 
     return likely_spacegroups
+    
 
+def run_pointless(dir_name, hklinp="XDS_ASCII.HKL"):
+    # Run pointless and extract pointgroup and spacegroup determination
+    # from its xmlout file.
+    cmline = "pointless XDSIN %s XMLOUT XDS_pointless.xml"
+    cmline += " HKLOUT XDS_pointless.mtz > XDS_pointless.log"
+    os.chdir(dir_name)
+    os.system(cmline % hklinp)
+
+def pointless(dir_name, hklinp="XDS_ASCII.HKL"):
+    run_pointless(dir_name, hklinp)
+    return process_pointless_xml()
+    
 if __name__ == '__main__':
     import os
     from xml.dom import minidom
     if not is_pointless_installed():
         print "!!  Error. Pointless program doesn't seems to be properly installed."
     else:
-        pointless(".")
+        print process_pointless_xml()
+        #pointless(".")
