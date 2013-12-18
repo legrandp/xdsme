ee8ec04f5b8adb71ca4564f41d8141475c76d46a
Pierre Legrand <legrand.p@gmail.com>
2013-12-18 12:04:07 +0100

    Updates and bugs fixes in pointless.py xml interpretation.

--- XDS/pointless.py
+++ XDS/pointless.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "0.1.2"
+__version__ = "0.1.4"
 __author__ = "Pierre Legrand (pierre.legrand \at synchrotron-soleil.fr)"
-__date__ = "06-06-2010"
-__copyright__ = "Copyright (c) 2010 Pierre Legrand"
+__date__ = "17-12-2013"
+__copyright__ = "Copyright (c) 2010-2014 Pierre Legrand"
 __license__ = "New BSD http://www.opensource.org/licenses/bsd-license.php"
 
 import os
 from xml.dom import minidom
 
 def is_pointless_installed():
-    tmpfile = "/tmp/xdsme_pointless.xml"
+    tmpfile = "/tmp/xdsme_pointless.%s.xml" % os.getpid()
     cmline = "rm -f %s; pointless XMLOUT %s > /dev/null 2>&1"
     os.system(cmline % (tmpfile, tmpfile))
     if os.path.isfile(tmpfile):
-        return True
+        installed = True
     else:
-        return False
+        installed = False
     os.system("rm -f %s" % tmpfile)
+    return installed
 
-def process_pointless_xml():
-    xml_inp = "XDS_pointless.xml"
-    logf_name = "XDS_pointless.log"
+def process_pointless_xml(file_name_id):
+    xml_inp = file_name_id + ".xml"
+    logf_name = file_name_id + ".log" 
     pname = 'a', 'b', 'c', 'alpha', 'beta', 'gamma'
     likely_spacegroups = []
     prob_max = 0
@@ -42,8 +43,8 @@ def process_pointless_xml():
         dom = minidom.parse(xml_inp)
         cell = dom.getElementsByTagName('cell')[0]
         spg_list = dom.getElementsByTagName('SpacegroupList')[0]
-        lattice_sym = dom.getElementsByTagName('LatticeSymmetry')[0]
-        new_cell = lattice_sym.getElementsByTagName('cell')[0]
+        #lattice_sym = dom.getElementsByTagName('LatticeSymmetry')[0]
+        #new_cell = lattice_sym.getElementsByTagName('cell')[0]
     except:
         os.chdir("..")
         raise
@@ -53,7 +54,6 @@ def process_pointless_xml():
     try:
         zone_list = dom.getElementsByTagName('ZoneScoreList')[0]
     except:
-        print "ERROR, here..."
         pass
     # Looking at systematique extinctions
     if zone_list:
@@ -80,7 +80,7 @@ def process_pointless_xml():
         sys_abs_prob = get_elem(node, 'SysAbsProb', float)
         spg_name = get_elem(node, 'SpacegroupName', str)
         spg_num = get_elem(node, 'SGnumber', int)
-        reidx_mat = map(int, get_elem(node, 'ReindexMatrix', str).split())
+        reidx_mat = map(float, get_elem(node, 'ReindexMatrix', str).split())
         all_dat = (spg_name, spg_num, total_prob, sys_abs_prob, reidx_mat)
         prob_max = max(total_prob, prob_max)
         print "%11s   #%d  %9.3f    %9.3f   %s" % all_dat
@@ -92,21 +92,21 @@ def process_pointless_xml():
 def run_pointless(dir_name, hklinp="XDS_ASCII.HKL"):
     # Run pointless and extract pointgroup and spacegroup determination
     # from its xmlout file.
-    cmline = "pointless XDSIN %s XMLOUT XDS_pointless.xml"
-    cmline += " HKLOUT XDS_pointless.mtz > XDS_pointless.log"
+    tmp_fn = "XDS_pointless" # + ".%s" % os.getpid()
+    cmline = "pointless XDSIN %s XMLOUT %s.xml" % (hklinp, tmp_fn)
+    cmline += " HKLOUT XDS_pointless.mtz > %s.log" % (tmp_fn)
     cmline += " <<EOF \nSETTING C2\nEOF"
     os.chdir(dir_name)
-    os.system(cmline % hklinp)
+    os.system(cmline)
+    return tmp_fn
 
 def pointless(dir_name, hklinp="XDS_ASCII.HKL"):
-    run_pointless(dir_name, hklinp)
-    return process_pointless_xml()
+    fname = run_pointless(dir_name, hklinp)
+    return process_pointless_xml(fname)
 
 if __name__ == '__main__':
-    import os
-    from xml.dom import minidom
     if not is_pointless_installed():
-        print "!!  Error. Pointless program doesn't seems to be properly installed."
+        print "ERROR: Pointless program doesn't seems to be installed."
     else:
-        print process_pointless_xml()
-        #pointless(".")
+        #print process_pointless_xml()
+        print pointless(".")
